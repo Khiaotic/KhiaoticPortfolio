de49bd66eff7075b0916d0bfe3bb593e81b8c2f6
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2023-03-13 19:22:39 -0400

    hero section and  some base css

--- khiaotic-temp-portfolio/src/App.js
+++ khiaotic-temp-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {NavBar, Navbar} from './components/NavBar';
+import {Hero} from './components/Hero';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Hero/>
     </div>
   );
 }
